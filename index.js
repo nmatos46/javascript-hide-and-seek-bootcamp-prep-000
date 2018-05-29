1a2cce089962f625c9943bb931a7a2b9b771a820
Norman Matos <nmatos419@hotmail.com>
2018-05-29 02:31:32 +0000

    Done.

--- index.js
+++ index.js
@@ -18,5 +18,15 @@ function increaseRankBy(n){
 }
 
 function deepestChild(){
+  var parent = document.querySelector('#grand-node')
   
+  //implement a depth first search (Assumption: no child has more than 1 child)
+  while (parent.children.length > 0) {
+    //continue looping while parent has a child
+    
+    //search for children in the first child of parent
+    parent = parent.children[0]
+  }
+  
+  return parent
 }
