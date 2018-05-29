2f212414a81c79f878826d53f23df78c4f42aa3e
Norman Matos <nmatos419@hotmail.com>
2018-05-29 02:02:50 +0000

    Done.

--- index.js
+++ index.js
@@ -3,5 +3,17 @@ function getFirstSelector(selector){
 }
 
 function nestedTarget(){
+  return document.querySelector('#nested').querySelector('.target')
+}
+
+function increaseRankBy(n){
+  //get all li elements in an element with class "ranked-list"
+  var ranks = document.getElementsByClassName('ranked-list').querySelectorAll('li')
+  
+  //iterate over all li elements to change them
+  for (var i=0; i<ranks.length; i++){
+    ranks[i].innerHTML = parseInt(ranks[i].innerHTML) + n
+  }
+  
   
 }
