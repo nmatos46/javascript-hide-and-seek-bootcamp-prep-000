49c0b364204eb3423482877f560becb0894d1a66
Norman Matos <nmatos419@hotmail.com>
2018-05-29 04:17:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function increaseRankBy(n){
   //get all ul elements with class "ranked-list"
   var ranks = document.getElementsByClassName('ranked-list')
   
-  //iterate over all ul elements to change them
+  //iterate over all ul elements to change their html
   for (var i=0; i<ranks.length; i++){
     ranks[i].innerHTML = parseInt(ranks[i].innerHTML) + n
   }
